50ddde8cdd5a2d3d0c5efdfadbb40ce3dd97de9d
NeymarL <liuhe@bingyan.net>
2016-02-24 20:13:07 +0800

    add auto change font-size

--- monitor.php
+++ monitor.php
@@ -134,12 +134,12 @@ function display($all_anchors, $memcache, $game_type)
             foreach ($type_anchors["anchors"] as $anchor) {
                 $state = $memcache->get($anchor["name"]);
                 if (empty($state) || $state == 0) {
-                    $offlne .= '<div><span style="width:170px;float:left;">' . $anchor["name"] . '</span><span style="color:#666666;width:40px;"><strong>离线</strong></span><span style="margin-left:25px;">
+                    $offlne .= '<div><span style="width:170px;float:left;font-size:2.2vw">' . $anchor["name"] . '</span><span style="color:#666666;width:40px;"><strong>离线</strong></span><span style="margin-left:25px;font-size:2.2vw">
                         ' . $anchor["url"] . '</span></div>';
                 } else {
                     $online .= '<div><span style="width:170px;float:left;"><strong>' . $anchor["name"] . '</strong></span>
                             <span style="color:red;width:40px;"><strong>直播</strong></span>
-                            <span style="margin-left:25px; font-size:2vw">' . $anchor["url"] . '</span></div>';
+                            <span style="margin-left:25px; font-size:2.2vw">' . $anchor["url"] . '</span></div>';
                 }
             }
         }
