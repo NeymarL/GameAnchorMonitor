7b3ace13c4ee2171da39328dd3fe4ee674cce6e2
NeymarL <liuhe@bingyan.net>
2016-02-24 15:44:39 +0800

    fixed game type

--- monitor.php
+++ monitor.php
@@ -112,11 +112,11 @@ function display($all_anchors, $memcache, $game_type)
         echo '<a class="control-item ui-link" href="dota2.php"><strong>DOTA2</strong></a>
             <a class="control-item ui-link" href="lol.php"><strong>LOL</strong></a>
             <a class="control-item ui-link active" href="hs.php"><strong>炉石</strong> </a>';
-    } else if ($game_type == "lol") {
+    } else if ($game_type == "LOL") {
         echo '<a class="control-item ui-link" href="dota2.php"><strong>DOTA2</strong></a>
             <a class="control-item ui-link active" href="lol.php"><strong>LOL</strong></a>
             <a class="control-item ui-link" href="hs.php"><strong>炉石</strong> </a>';
-    } else if ($game_type == "dota2") {
+    } else if ($game_type == "DOTA2") {
         echo '<a class="control-item ui-link active" href="dota2.php"><strong>DOTA2</strong></a>
             <a class="control-item ui-link" href="lol.php"><strong>LOL</strong></a>
             <a class="control-item ui-link" href="hs.php"><strong>炉石</strong> </a>';
@@ -130,10 +130,10 @@ function display($all_anchors, $memcache, $game_type)
             foreach ($type_anchors["anchors"] as $anchor) {
                 $state = $memcache->get($anchor["name"]);
                 if (empty($state) || $state == 0) {
-                    $offlne .= '<div><span style="width:85px;float:left;">' . $anchor["name"] . '</span><span style="color:#666666;width:40px;"><strong>离线</strong></span><span style="margin-left:25px;">
+                    $offlne .= '<div><span style="width:125px;float:left;">' . $anchor["name"] . '</span><span style="color:#666666;width:40px;"><strong>离线</strong></span><span style="margin-left:25px;">
                         ' . $anchor["url"] . '</span></div>';
                 } else {
-                    $online .= '<div><span style="width:85px;float:left;"><strong>' . $anchor["name"] . '</strong></span>
+                    $online .= '<div><span style="width:125px;float:left;"><strong>' . $anchor["name"] . '</strong></span>
                             <span style="color:red;width:40px;"><strong>直播</strong></span>
                             <span style="margin-left:25px;">' . $anchor["url"] . '</span></div>';
                 }
